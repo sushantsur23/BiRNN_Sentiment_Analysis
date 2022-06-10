cbe5de040c3301fd082f00142e62619439ab9b5e
sushantsur23 <sushant.sur23@gmail.com>
2022-06-10 17:31:09 +0530

    custom logs.py updated

--- src/utils/Logs.py
+++ src/utils/Logs.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 import logging
-
+import os
 
 class Logger():
-    logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
-    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-    filemode="a"
-    )
+
+    def logg():
+        logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+        log_dir = "logs"
+        os.makedirs(log_dir, exist_ok=True)
+        logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
